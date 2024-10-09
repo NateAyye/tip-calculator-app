924f85dab60e3e601b2b894174ed6f373492c160
Nathan Cuevas <nathanacuevas97@gmail.com>
2024-10-09 13:01:20 -0700

    Added shadcn/ui with some components aswell

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default config;
