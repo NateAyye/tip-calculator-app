c95d1beadd57736bd8bb11e4ea77d824a6aaab0e
Nathan Cuevas <nathanacuevas97@gmail.com>
2024-10-09 21:36:59 -0700

    adding Calendar Link

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Dashboard: React.FC = ({}) => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Link href="/calendar">Calendar</Link>
     </div>
   );
 };
